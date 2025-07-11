c79a3d38eb58fecda8d3c01034cffb34f711d224
Onur Ege <egeonur2121@gmail.com>
2025-07-11 16:36:12 +0300

    skills part created

---
+++ src/components/Skills.js
@@ -0,0 +1,66 @@
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png"
+import { Container, Row, Col } from "react-bootstrap";
+
+export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+    };
+
+    return(
+        <section className="skill" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="skill-bx">
+                            <h2>
+                                Skills
+                            </h2>
+                            <p>I specialize in developing web applications, mobile experiences, and applying machine learning to solve real-world problems. Good at Java, Kotlin, Python, and React. I build efficient APIs with Spring Boot and craft user-friendly Android apps using Jetpack Compose. My skills also includes working with Neo4j for graph data science, developing recommendation systems. I strive to create performant, maintainable, and impactful software.</p>
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                                <div className="item">
+                                    <img src={meter1} alt="Image"/>
+                                    <h5>Web & Backend Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image"/>
+                                    <h5>Mobile Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image"/>
+                                    <h5>Application Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter3} alt="Image"/>
+                                    <h5>ML & Data Science</h5>
+                                </div>
+                            </Carousel> 
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-left" src={colorSharp}/>
+        </section>
+    );
+}
